82e8721305e13ceaad200428aa170460d090d33f
Boris <banofon@gmail.com>
2022-11-30 09:53:15 -0600

    test: -added a login test

--- Friends-Storyboard/Login/Controller/LoginViewController.swift
+++ Friends-Storyboard/Login/Controller/LoginViewController.swift
@@ -13,10 +13,17 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var errorMassgeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        errorMassgeLabel.isHidden = true
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        resetUI()
     }
     
     
@@ -28,9 +35,43 @@ class LoginViewController: UIViewController {
         //i did not perform the check because i dont have the api yet
         // SO clicking to the login button goes directly to the homescreen
         
-        
-        performSegue(withIdentifier: "goToHome", sender: self)
-        
+        if isValidInput(){
+            performSegue(withIdentifier: "goToHome", sender: self)
+        }
     }
 
+}
+
+// Test
+extension LoginViewController {
+    func isValidInput() -> Bool {
+        
+        guard let username = userNameTextField.text, let password = passwordTextField.text else{
+            assertionFailure("Username/ password should never be nil")
+            return false
+        }
+        
+        if username.isEmpty || password.isEmpty {
+            errorMassgeLabel.isHidden = false
+            errorMassgeLabel.text =  "Username / password cannot be blank"
+            return false
+        }
+        if username == "Kevin" && password == "Kevin1234" {
+            return true
+        } else {
+            errorMassgeLabel.text =  "Username / password wrong"
+            errorMassgeLabel.isHidden = false
+            return false
+        }
+    }
+}
+
+// UI
+extension LoginViewController{
+    func resetUI(){
+        errorMassgeLabel.isHidden = true
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+        
+    }
 }
